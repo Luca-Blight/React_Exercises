ee9857b5b091bb863355e07995017fe24b35718c
Zack Royals <royalszachary@gmail.com>
2021-11-06 23:27:51 -0400

    Signficant updates

--- Projects/Web3_Projects/frontend/react_app/src/index.js
+++ Projects/Web3_Projects/frontend/react_app/src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css'
-import Counter from './components/counter';
+import App from "./App";
+import Counters from './components/counters';
 
-ReactDOM.render(<Counter />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
